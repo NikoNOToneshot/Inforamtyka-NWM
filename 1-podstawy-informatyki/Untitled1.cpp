b9b79cb81725dfe48cd3be081c7797fc165fe653
NikoNOToneshot <bogkebabgmail.com>
2025-10-31 13:16:40 +0100

    hamburber

---
+++ 1-podstawy-informatyki/Untitled1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a = 6;
+	int b = 3;
+	
+	
+	int suma = a + b;
+	int rurznica = a - b;
+	int iloczyn = a * b;
+	
+
+     cout << "Suma: " << suma << endl;
+    cout << "rurznica" << rurznica << endl;
+    cout << "Iloczyn: " << iloczyn << endl;
+
+
+
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ 1-podstawy-informatyki/typy danych.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "podaj swoje imie ";
+  
+   string i;
+   cin >> i;
+   
+  cout << "podaj swuj wiek " ;
+   short d;
+   cin >> d;
+   
+   
+   cout << "witaj " << i << "masz " << d << "lat "<< endl;
+    return 0;
+}
